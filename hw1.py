44c5f0d2e5ded9b0c4794b3310d6c6f8034e684a
kl938 <99301703+kl938@users.noreply.github.com>
2022-02-08 20:24:19 -0500

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -6,4 +6,13 @@ kl938@cornell.edu
 08 February 2022
 """
 
+import streamlit as st
+import pandas as pd
+
+"""Lines 13-17 from Streamlit "Getting Started" example"""
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
 
