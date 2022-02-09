63f363689f73d04f544d38ab7a7d1d58fd6c470c
kl938 <99301703+kl938@users.noreply.github.com>
2022-02-08 20:53:30 -0500

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -54,3 +54,13 @@ if uploaded_file is not None:
 image = Image.open('cornell.png')
 
 st.image(image, caption='Cornell University')
+
+"""From Streamlit "Layout Containers" example"""
+col1, col2 = st.columns([3, 1])
+data = np.random.randn(10, 1)
+
+col1.subheader("A wide column with a chart")
+col1.line_chart(data)
+
+col2.subheader("A narrow column with the data")
+col2.write(data)
