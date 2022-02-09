96edbb92539cc78848fbfc89b5fd43922eaaacc7
kl938 <99301703+kl938@users.noreply.github.com>
2022-02-08 20:49:23 -0500

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import datetime
+from PIL import Image
 
 """SYSEN5160"""
 """HW1"""
@@ -11,45 +12,45 @@ import datetime
 
 
 
-
-
-"""Lines 13-17 from Streamlit "Getting Started" example"""
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
-
-"""Lines 21-25 from Streamlit "Getting Started" example"""
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
-
-"""Lines 28-32 from Streamlit "Getting Started" example"""
+"""From Streamlit "Data Display" example"""
 dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
 
 st.dataframe(dataframe.style.highlight_max(axis=0))
-
-"""Lines 35-40 from Streamlit "Getting Started" example"""
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
-
-    chart_data
     
-"""Lines 45-48 from Streamlit "Data Display" example"""
+"""From Streamlit "Data Display" example"""
 col1, col2, col3 = st.columns(3)
 col1.metric("Bitcoin", "$50,000.00", "10%")
 col2.metric("Dogecoin", "$0.00001", "-10%")
 col3.metric("Sysen5130coin", "$10,000.00", "10%")
 
-"""Lines 51-54 from Streamlit "Input Widgets" example"""
+"""From Streamlit "Input Widgets" example"""
 d = st.date_input(
      "What day is it today?",
      datetime.date(2022, 8, 2))
 st.write('Today is:', d)
+
+"""From Streamlit "Input Widgets" example"""
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     # To read file as bytes:
+     bytes_data = uploaded_file.getvalue()
+     st.write(bytes_data)
+
+     # To convert to a string based IO:
+     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+     st.write(stringio)
+
+     # To read file as string:
+     string_data = stringio.read()
+     st.write(string_data)
+
+     # Can be used wherever a "file-like" object is accepted:
+     dataframe = pd.read_csv(uploaded_file)
+     st.write(dataframe)
+    
+"""From Streamlit "Media Elements" example"""
+image = Image.open('sunrise.jpg')
+
+st.image(image, caption='Sunrise by the mountains')
